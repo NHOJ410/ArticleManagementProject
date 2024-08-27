efc4d8e7202150835f13131a2e23beece412f12c
John <ae411253@gmail.com>
2024-08-27 19:24:07 +0800

    完成註冊和登入頁面的靜態模板

--- src/views/login/LoginPage.vue
+++ src/views/login/LoginPage.vue
@@ -1,7 +1,112 @@
-<script setup></script>
+<script setup>
+import { User, Lock } from '@element-plus/icons-vue'
+import { ref } from 'vue'
+const isRegister = ref(true)
+</script>
 
 <template>
-  <div>登入和註冊頁面</div>
+  <el-row class="login-page">
+    <el-col :span="12" class="bg"></el-col>
+    <el-col :span="6" :offset="3" class="form">
+      <!-- 註冊頁面 -->
+      <el-form ref="form" size="large" autocomplete="off" v-if="isRegister">
+        <el-form-item>
+          <h1>文章管理系統 - 歡迎註冊</h1>
+        </el-form-item>
+        <el-form-item>
+          <el-input :prefix-icon="User" placeholder="請輸入用戶名"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-input
+            :prefix-icon="Lock"
+            type="password"
+            placeholder="請輸入密碼"
+          ></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-input
+            :prefix-icon="Lock"
+            type="password"
+            placeholder="請再次輸入密碼"
+          ></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button class="button" type="primary" auto-insert-space>
+            註冊
+          </el-button>
+        </el-form-item>
+        <el-form-item class="flex">
+          <el-link
+            type="primary"
+            :underline="false"
+            @click="isRegister = false"
+          >
+            ← 已經有帳號了 ? 點我回登入頁面
+          </el-link>
+        </el-form-item>
+      </el-form>
+
+      <!-- 登入頁面 -->
+      <el-form ref="form" size="large" autocomplete="off" v-else>
+        <el-form-item>
+          <h1>文章管理系統 - 歡迎登入</h1>
+        </el-form-item>
+        <el-form-item>
+          <el-input :prefix-icon="User" placeholder="請輸入用戶名"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-input
+            name="password"
+            :prefix-icon="Lock"
+            type="password"
+            placeholder="請輸入密碼"
+          ></el-input>
+        </el-form-item>
+        <el-form-item class="flex">
+          <div class="flex">
+            <el-checkbox>記住我</el-checkbox>
+            <el-link type="primary" :underline="false">忘記密碼？</el-link>
+          </div>
+        </el-form-item>
+        <el-form-item>
+          <el-button class="button" type="primary" auto-insert-space
+            >登入</el-button
+          >
+        </el-form-item>
+        <el-form-item class="flex">
+          <el-link type="primary" :underline="false" @click="isRegister = true">
+            沒有帳號嗎 ? 點我去註冊 →
+          </el-link>
+        </el-form-item>
+      </el-form>
+    </el-col>
+  </el-row>
 </template>
 
-<style scoped></style>
+<style lang="scss" scoped>
+.login-page {
+  height: 100vh;
+  background: linear-gradient(to right, #a7a4a4, #f3f2f2);
+  .bg {
+    background: url('@/assets/bg3.webp') no-repeat center / cover;
+    border-radius: 0 20px 20px 0;
+  }
+  .form {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    user-select: none;
+    .title {
+      margin: 0 auto;
+    }
+    .button {
+      width: 100%;
+    }
+    .flex {
+      width: 100%;
+      display: flex;
+      justify-content: space-between;
+    }
+  }
+}
+</style>
